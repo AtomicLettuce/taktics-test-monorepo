d4e5beadec2d5983403291762df0240e934229ca
Xavier Vives Marcus <xvivesm@gmail.com>
2024-12-08 17:36:49 +0100

    budget details

--- admin/src/app/assets/js/modules/budgets/controllers/budgets.detail.controller.js
+++ admin/src/app/assets/js/modules/budgets/controllers/budgets.detail.controller.js
@@ -3,28 +3,89 @@ export default class BudgetsController {
         this.BudgetService = BudgetService;
         this.budgetId = $stateParams.id;
         this.budget = null;
-        this.chapters = [];
+        this.newChapter = null;
+        this.showChapterForm = false;
         this.loadBudgetDetails();
     }
 
 
 
-    loadBudgetDetails() {       
+    loadBudgetDetails() {
         this.BudgetService.getBudget(this.budgetId)
-        .then((response) => {
-            this.budget= response.data;
-        })
-        .catch((error) => {
-          console.error('Error fetching budgets:', error);
-          this.error = 'Failed to load budgets. Please try again later.';
+            .then((response) => {
+                this.budget = response.data;
+                if (this.budget.listOfChapters) {
+                    this.budget.listOfChapters.forEach(chapter => {
+                        chapter.showAddBatchForm = false;
+                        chapter.newBatch = {};
+                        if (chapter.listOfBatches) {
+                            chapter.listOfBatches.sort((a, b) => a.rank - b.rank)
+                        }
+                    });
+                    this.budget.listOfChapters.sort((a, b) => a.rank - b.rank);
+                }
+                this.newChapter = { rank: 0, description: '', labourSaleCoefficient: 0, materialSaleCoefficient: 0, budgetId: this.budget.id };
+            })
+            .catch((error) => {
+                console.error('Error fetching budgets:', error);
+                this.error = 'Failed to load budgets. Please try again later.';
+            });
+    }
+
+    updateChapter(chapter) {
+        const { showAddBatchForm, newBatch, ...chapterData } = chapter;
+        this.BudgetService.updateChapter(chapterData).then((response) => {
+            this.loadBudgetDetails();
+        }).catch((error) => {
+            console.error('Error updating chapter:', error);
+            alert('Failed to update chapter.');
+        });
+    }
+    deleteChapter(chapterId) {
+        this.BudgetService.deleteChapter(chapterId).then((response) => {
+            this.loadBudgetDetails();
+        }).catch((error) => {
+            console.error('Error deleting chapter:', error);
+            alert('Failed to delete chapter.');
         });
     }
 
 
+    updateBatch(batch) {
+        this.BudgetService.updateBatch(batch).then(() => { this.loadBudgetDetails(); })
+        this.loadBudgetDetails();
+    }
+
+    deleteBatch(batchId) {
+        this.BudgetService.deleteBatch(batchId).then(() => { this.loadBudgetDetails(); })
+    }
+
+    showAddBatchForm(chapter) {
+        chapter.showAddBatchForm = true;
+        chapter.newBatch = { rank: 0, description: '', ammount: 0, labourCostImport: 0, materialCostImport: 0 };
+    }
+    // Add Batch
+    addBatch(chapter) {
+        var newBatch = chapter.newBatch;
+        newBatch.chapterId = chapter.id;
+        this.BudgetService.addBatch(newBatch);
+        this.loadBudgetDetails();
+    }
+
+    showAddChapterForm() {
+        this.showChapterForm = true;
+        this.newChapter = { rank: 0, description: '', labourSaleCoefficient: 0, materialSaleCoefficient: 0, budgetId: this.budget.id };
+    }
+
+    addChapter() {
+        this.showChapterForm = false;
+        this.BudgetService.addChapter(this.newChapter)
+        this.loadBudgetDetails();
+    }
 
 
 }
 
 
 
-BudgetsController.$inject = ['$stateParams','BudgetService'];
+BudgetsController.$inject = ['$stateParams', 'BudgetService'];
--- admin/src/app/assets/js/modules/budgets/services/budget.service.js
+++ admin/src/app/assets/js/modules/budgets/services/budget.service.js
@@ -15,10 +15,33 @@ export default class BudgetService {
 
 
   getBudget(budgetId){
-    this.$http.get(`${this.apiBaseUrl}/Chapters`); // We only do this to force the chapters to be updated. Somehow, afterRemote does not work (¿)
-    this.$http.get(`${this.apiBaseUrl}/Batches`); // We only do this to force the chapters to be updated. Somehow, afterRemote does not work (¿)
+    const a=this.$http.get(`${this.apiBaseUrl}/Chapters`); // We only do this to force the chapters to be updated. Somehow, afterRemote does not work (¿)
+    const b=this.$http.get(`${this.apiBaseUrl}/Batches`); // We only do this to force the chapters to be updated. Somehow, afterRemote does not work (¿)
+    console.log(a.response)
+    console.log(b.response);
     return this.$http.get(`${this.apiBaseUrl}/Budgets/${budgetId}`);
   }
+
+  updateChapter(chapter){
+    return this.$http.put(`${this.apiBaseUrl}/Chapters/${chapter.id}`, chapter);
+  }
+
+  deleteChapter(chapterId){
+    return this.$http.delete(`${this.apiBaseUrl}/Chapters/${chapterId}`)
+  }
+  updateBatch(batch){
+    return this.$http.put(`${this.apiBaseUrl}/Batches/${batch.id}`, batch);
+  }
+  deleteBatch(batchId){
+    return this.$http.delete(`${this.apiBaseUrl}/Batches/${batchId}`)
+  }
+  addBatch(newBatch){
+    return this.$http.post(`${this.apiBaseUrl}/Batches`, newBatch)
+  }
+  addChapter(newChapter){
+    return this.$http.post(`${this.apiBaseUrl}/Chapters`, newChapter)
+  }
+
 }
 
 BudgetService.$inject = ['$http'];
